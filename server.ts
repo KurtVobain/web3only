17a0dd860b68b93fd453bb7e05f0fa1c35e90a3a
Evgenii Karpov <evgenii@WIN-U9CO2USQB8S>
2024-08-22 14:18:58 +0200

    Initial commit

---
+++ server.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+import puppeteer from 'puppeteer';
+import path from 'path';
+
+const app = express();
+
+// Serve the index.html file
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Screenshot endpoint
+app.get('/screenshot', async (req: Request, res: Response) => {
+    const url = req.query.url as string;
+    if (!url) {
+        return res.status(400).send('URL is required');
+    }
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'networkidle2' });
+        const screenshot = await page.screenshot();
+        await browser.close();
+
+        res.set('Content-Type', 'image/png');
+        res.send(screenshot);
+    } catch (error) {
+        console.error('Error taking screenshot:', error);
+        res.status(500).send('Failed to generate screenshot');
+    }
+});
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
